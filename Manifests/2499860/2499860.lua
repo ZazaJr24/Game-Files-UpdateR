97151d51dfdd7ed90d9c2dbf3a846d1fe41b0da8
ZazaJr24 <kaanleon2462@gmail.com>
2026-07-05 19:39:36 +0200

    Add DRAGON QUEST VII Reimagined Lua script

---
+++ Manifests/2499860/2499860.lua
@@ -0,0 +1,7 @@
+-- 2499860's Lua and Manifest
+-- DRAGON QUEST VII Reimagined
+-- Created by Dark Tides Worker
+-- Total Depots: 1
+
+-- MAIN APPLICATION
+addappid(2499861, 1, "78d85bb7957e80d4fac0655a65ff9bee61dafc06c753de2713726e9860d6a860") -- DRAGON QUEST VII Reimagined
