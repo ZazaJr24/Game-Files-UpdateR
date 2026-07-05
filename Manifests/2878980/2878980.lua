74cbee70cc715dcdbc56d4525fdbef6872fab46b
ZazaJr24 <kaanleon2462@gmail.com>
2026-07-05 20:06:23 +0200

    Add NBA 2K25 Lua script

---
+++ Manifests/2878980/2878980.lua
@@ -0,0 +1,8 @@
+-- 2878980's Lua and Manifest
+-- NBA 2K25
+-- Created by Dark Tides Worker
+-- Total Depots: 3
+
+-- MAIN APPLICATION
+addappid(2878981, 1, "df09d8fa7b88da65f6fdce275ab09b85e5c12731cad10c147745c7501acb1f14") -- NBA 2K25
+addappid(3107390, 1, "a86b0e16a6d6f9946fc2671411ad8653123970931a5e289b242e3b1cf563381f") -- Depot 3107390
