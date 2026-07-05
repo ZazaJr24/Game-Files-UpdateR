154dda37517cb26479b571f1331219ee2a29c030
ZazaJr24 <kaanleon2462@gmail.com>
2026-07-05 20:08:28 +0200

    Add Hogwarts Legacy Lua script

---
+++ Manifests/990080/990080.lua
@@ -0,0 +1,15 @@
+-- 990080's Lua and Manifest
+-- Hogwarts Legacy
+-- Created by Dark Tides Worker
+-- Total Depots: 9
+
+-- MAIN APPLICATION
+addappid(990081, 1, "99edd7f3221635d93481770b28ebac240488e1f42cf16f01e5a4e744b0944da6") -- Hogwarts Legacy
+addappid(990082, 1, "ac55581fbdef1f6687f91654d435e2d4c395d2fa56eb5aba4fa52d708118157e") -- Depot 990082
+addappid(990083, 1, "2326f907ccd6ebfc8235495fbb2abd7b12d814a3228cd3b0bf480f7c602c87f5") -- Depot 990083
+addappid(990084, 1, "f043f2ca0cfbbaf5fcb6e30d4beb1a20ba5ef293db11dc4e5521bdfa8a7fdfe0") -- Depot 990084
+addappid(990085, 1, "b7d07ff314786df30f9d32266a2e88cd026fabaffbd2567dbc1637cd319ef996") -- Depot 990085
+addappid(990086, 1, "d9814846faaa5f83e45151bec5190062e320c6b9a6842e7722ed51293a29a667") -- Depot 990086
+addappid(990087, 1, "42c46e0e1520cefc9f4120f00fca640bc83f48b322e7b19ad9530538d9cbee75") -- Depot 990087
+addappid(990088, 1, "81c364d84279c2fafb7960d0aabd75e7e371ad27f275484d99ef5734e4c00d95") -- Depot 990088
+addappid(990089, 1, "21886ab063a78ccf9b676f311597b895c73a1410cd9f7e0e3e7b4e99b82f1e6b") -- Depot 990089
