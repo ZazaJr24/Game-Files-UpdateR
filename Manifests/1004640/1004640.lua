ea64b008fd2149c3e75ac1a72656b87a4cdfbe8a
ZazaJr24 <kaanleon2462@gmail.com>
2026-07-05 05:35:04 +0200

    Add FINAL FANTASY TACTICS - The Ivalice Chronicles Lua script

---
+++ Manifests/1004640/1004640.lua
@@ -0,0 +1,7 @@
+-- 1004640's Lua and Manifest
+-- FINAL FANTASY TACTICS - The Ivalice Chronicles
+-- Created by Dark Tides Worker
+-- Total Depots: 1
+
+-- MAIN APPLICATION
+addappid(1004641, 1, "b9ffdc9182829343bcdf50e8c3fe66b2fc58fbbe11980078cfc04e6f8d91701d") -- FINAL FANTASY TACTICS - The Ivalice Chronicles
