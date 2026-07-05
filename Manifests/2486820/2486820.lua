2bd4c2a628b8132a2c5ec50d9df25ec01e334182
ZazaJr24 <kaanleon2462@gmail.com>
2026-07-05 19:39:54 +0200

    Add Sonic Racing: CrossWorlds Lua script

---
+++ Manifests/2486820/2486820.lua
@@ -0,0 +1,7 @@
+-- 2486820's Lua and Manifest
+-- Sonic Racing: CrossWorlds
+-- Created by Dark Tides Worker
+-- Total Depots: 1
+
+-- MAIN APPLICATION
+addappid(2486821, 1, "08e2539b55bc79fefa9447d3479c57b481c4256c12ebd50529fc3722fcc3727f") -- Sonic Racing: CrossWorlds
