20f6a8ad190ada8c25a50b74a5adb6edc4f855cb
ZazaJr24 <kaanleon2462@gmail.com>
2026-07-05 05:36:53 +0200

    Add Middle-earth: Shadow of War Lua script

---
+++ Manifests/356190/356190.lua
@@ -0,0 +1,7 @@
+-- 356190's Lua and Manifest
+-- Middle-earth: Shadow of War
+-- Created by Dark Tides Worker
+-- Total Depots: 5
+
+-- MAIN APPLICATION
+addappid(356191, 1, "260abd037eed772bf36b780e13d94ab075cc04f2f80fa4f3e39b907c037892ae") -- Middle-earth: Shadow of War
