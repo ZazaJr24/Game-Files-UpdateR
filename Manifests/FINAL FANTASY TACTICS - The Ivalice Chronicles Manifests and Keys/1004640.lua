6f42aad73e873a6870efc8d9c66b0ee42a3820c3
ZazaJr24 <kaanleon2462@gmail.com>
2026-07-05 11:12:32 +0200

    Update 1004640.lua

---
+++ Manifests/FINAL FANTASY TACTICS - The Ivalice Chronicles Manifests and Keys/1004640.lua
@@ -0,0 +1,14 @@
+-- 1004640's Lua and Manifest Created by Hubcap Manifest
+-- FINAL FANTASY TACTICS - The Ivalice Chronicles
+-- Created: June 16, 2026 at 22:04:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(1004640, 1, "bd74df1e233ffd3920f8bbf8fe4dc604f0793b026dea9d94c6ea296b7b8df896") -- FINAL FANTASY TACTICS - The Ivalice Chronicles
+-- MAIN APP DEPOTS
+addappid(1004641, 1, "b9ffdc9182829343bcdf50e8c3fe66b2fc58fbbe11980078cfc04e6f8d91701d") -- Depot 1004641
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3589750) -- FINAL FANTASY TACTICS - The Ivalice Chronicles Pre-order Bonuses
+addappid(3589760) -- FINAL FANTASY TACTICS - The Ivalice Chronicles Deluxe Edition Bonuses
