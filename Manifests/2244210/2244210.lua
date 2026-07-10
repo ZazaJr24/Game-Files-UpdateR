e7c7037c89cd2d8000e2bff53d2a21af89b8c87b
ZazaJr24 <kaanleon2462@gmail.com>
2026-07-10 12:34:29 +0200

    Update 2244210.lua

---
+++ Manifests/2244210/2244210.lua
@@ -0,0 +1,31 @@
+-- 2244210's Lua and Manifest Created by Hubcap Manifest
+-- Echoes of Aincrad
+-- Created: July 10, 2026 at 01:38:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 8
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2244210) -- Echoes of Aincrad
+-- MAIN APP DEPOTS
+addappid(2244211, 1, "105c2f87ad76d3549179dfbeb54c0248a1bde5a72ba14d74714e480d4706f57e") -- Depot 2244211
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+-- DLCS WITH DEDICATED DEPOTS
+-- Echoes of Aincrad Unansweredbutterfly Bonus Contents App (AppID: 3443750)
+addappid(3443750)
+addappid(3443750, 1, "85140ce089e2a2c387357acfbc4408d975400ab69786255011f7a1d0bc406d2d") -- Echoes of Aincrad Unansweredbutterfly Bonus Contents App - Depot 3443750
+-- Echoes of Aincrad - Anime Unansweredbutterfly (HD Version) (AppID: 3996330)
+addappid(3996330)
+addappid(3996330, 1, "c3328094b1d75686feb7c847e767f15dd73e268e700d36090b8e8b9052aa0900") -- Echoes of Aincrad - Anime Unansweredbutterfly (HD Version) - Depot 3996330
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4249240) -- Echoes of Aincrad - Death Game Mode Early Unlock
+addappid(4249250) -- Echoes of Aincrad - Weapon Pack Proto-Elucidator Series
+addappid(4249260) -- Echoes of Aincrad - Armor Pack Flutter Boots  Flutter Robes
+addappid(4249270) -- Echoes of Aincrad - Starter Pack
+addappid(4337740) -- Echoes of Aincrad - Expansion Pack
+addappid(4337770) -- Echoes of Aincrad - Ultimate Upgrade Pack
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2244212) -- Depot 2244212 (empty depot)
