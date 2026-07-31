7254891ba35f081a0df69b05a6717b60219d99ea
ZazaJr24 <kaanleon2462@gmail.com>
2026-07-31 16:28:28 +0200

    Update 3751950.lua

--- Manifests/3751950/3751950.lua
+++ Manifests/3751950/3751950.lua
@@ -1,29 +1,49 @@
-addappid(3751950)
-addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-setManifestid(1716751, "8936186649444731600", 0)
-addappid(3751951, 1, "0495628add2f29892c7a7930e1ed5332f68e374d7621e3338f23492fcd8f23db")
-setManifestid(3751951, "6244593628330629913", 0)
+-- 3751950's Lua and Manifest Created by Hubcap Manifest
+-- Assassin's Creed Black Flag Resynced
+-- Created: July 22, 2026 at 14:10:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 10
+-- Total DLCs: 24
+-- Shared Depots: 2
 
-
-addappid(4496490) -- Assassin's Creed Black Flag Resynced - Master Assassin Character Pack
-addappid(4496500) -- Assassin's Creed Black Flag Resynced - Master Assassin Character Pack - Ubisoft Activation
-addappid(4496510) -- Assassin's Creed Black Flag Resynced - Master Assassin Naval Pack
-addappid(4496520) -- Assassin's Creed Black Flag Resynced - Master Assassin Naval Pack - Ubisoft Activation
-addappid(4496530) -- Assassin's Creed Black Flag Resynced - Hellfire Character Pack
-addappid(4496540) -- Assassin's Creed Black Flag Resynced - Hellfire Character Pack - Ubisoft Activation
-addappid(4496550) -- Assassin's Creed Black Flag Resynced - Hellfire Naval Pack
-addappid(4496560) -- Assassin's Creed Black Flag Resynced - Hellfire Naval Pack - Ubisoft Activation
-addappid(4496580) -- Assassin's Creed Black Flag Resynced - Sea Serpent Character Pack
-addappid(4496590) -- Assassin's Creed Black Flag Resynced - Sea Serpent Character Pack - Ubisoft Activation
-addappid(4496600) -- Assassin's Creed Black Flag Resynced - Sea Serpent Naval Pack
-addappid(4496610) -- Assassin's Creed Black Flag Resynced - Sea Serpent Naval Pack - Ubisoft Activation
-addappid(4496620) -- Assassin's Creed Black Flag Resynced - Dragon Storm Character Pack
-addappid(4496630) -- Assassin's Creed Black Flag Resynced - Dragon Storm Character Pack - Ubisoft Activation
-addappid(4496640) -- Assassin's Creed Black Flag Resynced - Dragon Storm Naval Pack
-addappid(4496650) -- Assassin's Creed Black Flag Resynced - Dragon Storm Naval Pack - Ubisoft Activation
-addappid(4496660) -- Assassin's Creed Black Flag Resynced - Map Pack
-addappid(4496670) -- Assassin's Creed Black Flag Resynced - MAP PACK - Ubisoft Activation
-addappid(4519940) -- Assassin's Creed Black Flag Resynced - Standard Edition - Prepurchase - Ubisoft Activation
-addappid(4519950) -- Assassin's Creed Black Flag Resynced - Deluxe Edition - Prepurchase - Ubisoft Activation
-addappid(4872930) -- Assassin's Creed Black Flag Resynced - Standard Edition - PREVIEW - Ubisoft Activation
-addappid(4892480) -- Assassin's Creed Black Flag Resynced - Deluxe Edition - PREVIEW - Ubisoft Activation
+-- MAIN APPLICATION
+addappid(3751950, 1, "e6d96386c77349411f5be39f2955ee0649a9a80abd22c3d69752bfc6e3302539") -- Assassin's Creed Black Flag Resynced
+-- MAIN APP DEPOTS
+addappid(3751951, 1, "0495628add2f29892c7a7930e1ed5332f68e374d7621e3338f23492fcd8f23db") -- Depot 3751951
+addappid(3751953, 1, "f96dc68fd865006d30d7d804bab42593f8bb6a5250624df45771cbc2a9c7aa45") -- Depot 3751953
+addappid(3751954, 1, "b61e5b022604f5f47312b82b8489487c5b1dc57159c8ee09ecac0c7959631762") -- Depot 3751954
+addappid(3751955, 1, "f2ae043d5d7111cfd549ed1bd3f0611cd26711aa4a55e1af56889e43e8d0fb1c") -- Depot 3751955
+addappid(3751956, 1, "c6c9dba85bf9b4a6f19551cc87b047ab631cd88317027eb33e30174e4bbe41fc") -- Depot 3751956
+addappid(3751957, 1, "c98ba5f83bc7269be83bedef8689d8a68ff2bb4ad1d6f7ba221178305af557c4") -- Depot 3751957
+addappid(3751958, 1, "17b0d6cff686c37972334878f48648f8982f40f290fdaeddc022e1a795e6452c") -- Depot 3751958
+addappid(3751959, 1, "6011c824af6fe4db62d3154d751de76d49422e4a56218d54d8ceb716cac7fa6c") -- Depot 3751959
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") -- Ubisoft Connect PC Client Content (Shared from App 1716750)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4496490) -- Assassins Creed Black Flag Resynced - Master Assassin Character Pack
+addappid(4496500) -- Assassins Creed Black Flag Resynced - Master Assassin Character Pack - Ubisoft Activation
+addappid(4496510) -- Assassins Creed Black Flag Resynced - Master Assassin Naval Pack
+addappid(4496520) -- Assassins Creed Black Flag Resynced - Master Assassin Naval Pack - Ubisoft Activation
+addappid(4496530) -- Assassins Creed Black Flag Resynced - Hellfire Character Pack
+addappid(4496540) -- Assassins Creed Black Flag Resynced - Hellfire Character Pack - Ubisoft Activation
+addappid(4496550) -- Assassins Creed Black Flag Resynced - Hellfire Naval Pack
+addappid(4496560) -- Assassins Creed Black Flag Resynced - Hellfire Naval Pack - Ubisoft Activation
+addappid(4496580) -- Assassins Creed Black Flag Resynced - Sea Serpent Character Pack
+addappid(4496590) -- Assassins Creed Black Flag Resynced - Sea Serpent Character Pack - Ubisoft Activation
+addappid(4496600) -- Assassins Creed Black Flag Resynced - Sea Serpent Naval Pack
+addappid(4496610) -- Assassins Creed Black Flag Resynced - Sea Serpent Naval Pack - Ubisoft Activation
+addappid(4496620) -- Assassins Creed Black Flag Resynced - Dragon Storm Character Pack
+addappid(4496630) -- Assassins Creed Black Flag Resynced - Dragon Storm Character Pack - Ubisoft Activation
+addappid(4496640) -- Assassins Creed Black Flag Resynced - Dragon Storm Naval Pack
+addappid(4496650) -- Assassins Creed Black Flag Resynced - Dragon Storm Naval Pack - Ubisoft Activation
+addappid(4496660) -- Assassins Creed Black Flag Resynced - Map Pack
+addappid(4496670) -- Assassins Creed Black Flag Resynced - MAP PACK - Ubisoft Activation
+addappid(4496720) -- Assassins Creed Black Flag Resynced - Standard Edition - Ubisoft Activation
+addappid(4496730) -- Assassins Creed Black Flag Resynced - Deluxe Edition - Ubisoft Activation
+addappid(4519940) -- Assassins Creed Black Flag Resynced - Standard Edition - Prepurchase - Ubisoft Activation
+addappid(4519950) -- Assassins Creed Black Flag Resynced - Deluxe Edition - Prepurchase - Ubisoft Activation
+addappid(4872930) -- Assassins Creed Black Flag Resynced - Standard Edition - PREVIEW - Ubisoft Activation
+addappid(4892480) -- Assassins Creed Black Flag Resynced - Deluxe Edition - PREVIEW - Ubisoft Activation
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3751952) -- Depot 3751952 (empty depot)
